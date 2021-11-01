8f92b5a6ec05935029aafa516d62cc81c65f139c
Mahesh Chandrashekar <mjmahi90@gmail.com>
2021-11-01 12:57:22 +0530

    Corrected the identifier name

--- qa-automation-ios-testUITests/AccessibilityIdentifiers/AccessibilityIdentifiers.swift
+++ qa-automation-ios-testUITests/AccessibilityIdentifiers/AccessibilityIdentifiers.swift
@@ -7,7 +7,7 @@
 //
 import Foundation
 
-public enum AccessibilityIndentifiers {
+public enum AccessibilityIdentifiers {
     
     enum Login {
         static let usernameTextField = "usernameTextField"
